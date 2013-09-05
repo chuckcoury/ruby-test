a92da13b0bbce09322428d6f4b0ae55da7800a05
Chuck Coury <chuck@coury.org>
2013-09-05 16:19:49 -0400

    science bitch

---
+++ hello.rb
@@ -0,0 +1,8 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1187\cocoasubrtf390
+{\fonttbl\f0\fnil\fcharset0 Verdana;}
+{\colortbl;\red255\green255\blue255;\red38\green38\blue38;\red255\green255\blue255;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl320
+
+\f0\fs22 \cf2 \cb3 puts "science bitch!"}
--- web.rb
+++ web.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
 
 get '/' do
-  "Hello, world"
+  "Science bitches!"
 end
